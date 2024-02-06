84f817b5eeee3ac05e015f61d9a293350c42de4a
Rahid <shaikrahid985@gmail.com>
2024-02-06 12:22:13 +0530

    commit

---
+++ src/useReducerHook-reducers/userRedcer.js
@@ -0,0 +1,24 @@
+const userReducer = (state, action) => {
+    switch (action.type) {
+      case 'GET_USER':
+        console.log(action.payload, "56789");
+        return { ...state, userDetails: action.payload };
+  
+      case 'UPDATE_DETAILS':
+        return {
+          ...state,
+          userDetails: {
+            ...state.userDetails,
+            [action.field]: action.value,
+          },
+        };
+      case 'CLEAR_DETAILS':
+        return {...state,userDetails:{}}
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
+  
