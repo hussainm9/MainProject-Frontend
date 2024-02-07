050022b069a5058c114a81c1966c5b3e1bb88e2d
hussainm9 <mehamoodm9@gmail.com>
2024-02-07 14:08:00 +0530

    resgetAll added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React,{useReducer,useEffect} from 'react'
-import axios from 'axios';
-import userContext from './contextApi/userContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useReducer } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './components/Login/Signin';
 import Registration from './components/Register Components/Register';
@@ -10,17 +8,19 @@ import AdminDashboard from './components/admin/AdminDashboard';
 import ForgotPassword from './components/forgotPassword/ForgotPassword';
 import ResetPassword from './components/forgotPassword/resetPassword';
 import GuestHome from './components/guestHome/Home';
-import userReducer from './useReducerHook-reducers/userRedcer';
-import RestaurantHome from './components/restaurantHome/Home';
+import Profile from './components/guestHome/profile';
 import Header from './components/header/Header';
 import AddTable from './components/pages/AddTable';
 import LandingPage from './components/pages/LandingPage';
 import RestaurantDashboard from './components/restaurantHome/Dashboard';
+import RestaurantHome from './components/restaurantHome/Home';
 import Rejected from './components/restaurantHome/Rejected';
 import ThankYou from './components/restaurantHome/ThankYou';
-import Profile from './components/guestHome/profile';
-
-
+import axiosInstance from './config/axios';
+import restaurantContext from './contextApi/restaurantContext';
+import userContext from './contextApi/userContext';
+import restaurantReducer from './useReducerHook-reducers/restaurantReducer';
+import userReducer from './useReducerHook-reducers/userRedcer';
 
 
 
@@ -28,16 +28,18 @@ import Profile from './components/guestHome/profile';
 
 function App() {
     const [userState, userDispatch] = useReducer(userReducer, { userDetails: {} });
+    const [restaurantState, restaurantDispatch] = useReducer(restaurantReducer, { restaurantOwner:{},allRestaurants: []});
+
 
     useEffect(() => {
         (async () => {
             try {
-                const userDetails = await axios.get('http://localhost:3786/api/user/profile', {
+                const userDetails = await axiosInstance.get('/api/user/profile', {
                     headers: {
                         Authorization: localStorage.getItem('token'),
                     },
                 });
-                console.log(userDetails.data);
+                console.log(userDetails.data._id, "235254325");
                 userDispatch({ type: 'GET_USER', payload: userDetails.data });
             } catch (e) {
                 console.log(e);
@@ -45,13 +47,50 @@ function App() {
         })();
     }, []);
 
+    useEffect(()=>{
+        if(Object.keys(userState.userDetails).length > 0){
+            (async ()=>{
+                try {
+                    const { data } = await axiosInstance.get(`/api/restaurant/${userState.userDetails._id}`, {
+                        headers: {
+                            "Authorization": localStorage.getItem('token')
+                        }
+                    })
+                    restaurantDispatch({ type: 'GET_RESTAURANT', payload:data })
+                }catch(err){
+                    console.log(err)
+                }
+    
+            })()
+        }
+    }, [userState.userDetails])
+    useEffect(()=>{
+        (async()=>{
+            try{
+                const {data} =await axiosInstance('/api/getAll',{
+                    headers:{
+                        Authorization:localStorage.getItem('token')
+                    }
+                })
+               // console.log(data);
+                restaurantDispatch({type:'GET_ALL',payload:data})
 
 
+
+            }catch(e){
+                console.log(e,'err in getAll res');
+            }
+        })()
+
+    },[])
+
+
+    console.log(restaurantState && restaurantState, "234")
     return (
         <BrowserRouter>
 
             <userContext.Provider value={{userState,userDispatch}}>
-
+            <restaurantContext.Provider value={{restaurantState, restaurantDispatch}}>
             <div>
                 {/* <h2>React Fundamentals</h2> */}
 
@@ -76,7 +115,7 @@ function App() {
                     <Route path='/restaurant/:restaurantId' element={<RestaurantDashboard />} />
                 </Routes>
             </div>
-
+            </restaurantContext.Provider>
             </userContext.Provider>
 
         </BrowserRouter>
--- src/components/pages/AddTable.js
+++ src/components/pages/AddTable.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useContext } from "react"
+import restaurantContext from "../../contextApi/restaurantContext"
+
 
 export default function AddTable() {
+  const { restaurantState } = useContext(restaurantContext)
+  console.log(restaurantState, "resofy")
+
   return (
     <div>
         <h2>Add table</h2>
